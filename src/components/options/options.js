0e8cec3df05e5370c271b844ead0a221bcf10f8c
Your NameJoseph <you@example.comjosephracca@gmail.com>
2021-01-26 09:11:57 -0800

    changes

--- src/components/options/options.js
+++ src/components/options/options.js
@@ -6,13 +6,12 @@ import "./options.css";
 // import FormField from "../formControl/formControl";
 import Music from "../../audio/All_Or_Nothing.mp3";
 
+import ReactAudioPlayer from "react-audio-player";
 
 // need to figure out how to do a .map on this one...
 
-
 // console.log(FieldGroup);
 class OptionsPage extends React.Component {
-
   render() {
     return (
       <div className="bgOpt slideUp height100">
@@ -20,40 +19,48 @@ class OptionsPage extends React.Component {
           <Row>
             <Col>
               <h2>OPTIONS</h2>
-              <p> MADLIB GEN </p>
             </Col>
           </Row>
 
           <Row className="mb-5">
-              <Col className="mb-5">
-                <figure>
-                  <span>
-                    <figcaption>
-                      <p>
-                        {" "}
-                        <i class="fas fa-volume-up"></i> + auditory stimulus!
-                      </p>
-                    </figcaption>
-                    <audio controls src={Music} autoplay>
-                      Your browser does not support the
-                      <code>audio</code> element.
-                    </audio>
-                  </span>
+            {/* <Col className="mb-5">
+              <figure>
+                <span>
                   <figcaption>
                     <p>
-                      <i class="fas fa-music"></i> by{" "}
-                      <a href="https://www.youtube.com/watch?v=NGv-ta9eFbs">
-                        Sanny D.
-                      </a>
+                      {" "}
+                      <i class="fas fa-volume-up"></i> + auditory stimulus!
                     </p>
                   </figcaption>
-                </figure>
-              </Col>
-            </Row>
+                  <audio controls src={Music} autoplay>
+                    Your browser does not support the
+                    <code>audio</code> element.
+                  </audio>
+                </span>
+                <figcaption>
+                  <p>
+                    <i class="fas fa-music"></i> by{" "}
+                    <a href="https://www.youtube.com/watch?v=NGv-ta9eFbs">
+                      Sanny D.
+                    </a>
+                  </p>
+                </figcaption>
+              </figure>
+            </Col> */}
+            <Col>
+              <ReactAudioPlayer src={Music} volume={.05} autoPlay controls={true} controlsList />
+              <p>
+                    <i class="fas fa-music"></i> by{" "}
+                    <a href="https://www.youtube.com/watch?v=NGv-ta9eFbs">
+                      Sanny D.
+                    </a>
+                  </p>
+            </Col>
+          </Row>
         </Container>
       </div>
     );
   }
 }
-
 export default OptionsPage;
+
